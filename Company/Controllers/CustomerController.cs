f23e3d2a1d4bdf21c31975e27c8ba26ead9a7b6f
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-13 19:40:12 +0000

    CONTROLLER LOGIC

---
+++ Company/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using Company.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Company.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ICustomerRepository repository;
+        public CustomerController(ICustomerRepository repo)
+        {
+            repository = repo;
+        }
+
+        //GET: Customer
+        public ActionResult Index()
+        {
+            return View(repository.GetCustomers());
+        }
+
+        //GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(repository.GetCustomerByID(id));
+        }
+    }
+}
