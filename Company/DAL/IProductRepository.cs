a8497e107ce8982b816d2c7da63a343f3b11dd23
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-14 13:22:16 +0000

    ProductRepository and IProductRepository

---
+++ Company/DAL/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Company.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Company.DAL
+{
+    public class IProductRepository
+    {
+        List<Product> GetProducts();
+        Product GetProductByID(int id);
+    }
+}
