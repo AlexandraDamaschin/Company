e1a8e96e8422337103d8968815be4caab8c6ab40
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-15 14:08:39 +0000

    controllers project works fine

---
+++ Company/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Company.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Company.Controllers
+{
+    public class ProductController : Controller
+    {
+        IProductRepository repository;
+
+        // GET: Product
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(repository.GetProducts());
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(repository.GetProductByID(id));
+        }
+    }
+    }
